75ceb7fc75fb93725bdc7fd674f488dcd9bd2634
jbh105 <jaydipbhayani105@gmail.com>
2024-04-30 23:24:04 +0530

    some changes

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ MongooseConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({
-    origin: 'http://localhost:3001' 
-}));
+app.use(cors());
 
 app.use('/',UserRouter);
 
