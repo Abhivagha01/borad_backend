8bb16bb0d5e64aee58d224e4a5cbd2c8cb433088
jbh105 <jaydipbhayani105@gmail.com>
2024-05-02 01:35:14 +0530

    add files

--- Connfig/Db.js
+++ Connfig/Db.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+
 
 async function connectToMongoDB() {
     try {
-        await mongoose.connect(process.env.MONGOOSE_CONNECT, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected successfully.');
+        await mongoose.connect(process.env.MONGOOSE_CONNECT)
+        console.log(`Successfully connected to : ${mongoose.connection.host}`);
     } catch (error) {
         console.error('MongoDB connection error:', error);
+        process.exit(1);
+
     }
 }
 
